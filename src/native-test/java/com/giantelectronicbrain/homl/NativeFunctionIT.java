f58fbe137e36e03ac172dcbfe399175d0203c7ce
tharter <tharter@giantelectronicbrain.com>
2021-11-28 19:53:25 -0800

    initial test of building

---
+++ src/native-test/java/com/giantelectronicbrain/homl/NativeFunctionIT.java
@@ -0,0 +1,9 @@
+package com.giantelectronicbrain.homl;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeFunctionIT extends FunctionTest {
+
+    // Execute the same tests but in native mode.
+}
