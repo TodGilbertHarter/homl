161f235679d6c682d11cadc5b29b5e09b9438845
tharter <tharter@giantelectronicbrain.com>
2023-07-27 14:29:30 -0700

    fixed character sheet issues, standalone character sheet will need to be reimplemented at some point if needed

--- scripts/generate_character.sh
+++ scripts/generate_character.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Generate the HoML character sheet and embedded character sheet
+# Generate the HoML embedded character sheet
 GEB_NAME="geb-$1"
 PUBDIR="geb/$GEB_NAME"
 mkdir -p ./build/$PUBDIR
 cd ./build/site
-hairball charsheet.hairball "$2_charactersheet.hairball" | tail -n +2 >"$2_charactersheet.html"
-hairball embedded_charsheet.hairball "empty_charactersheet.hairball" | tail -n +2 >"embedded_charactersheet.html"
+#hairball charsheet.hairball "$2_charactersheet.hairball" | tail -n +2 >"$2_charactersheet.html"
+#hairball embedded_charsheet.hairball "empty_charactersheet.hairball" | tail -n +2 >"embedded_charactersheet.html"
+hairball charsheet.hairball "empty_charactersheet.hairball" | tail -n +2 >"embedded_charactersheet.html"
